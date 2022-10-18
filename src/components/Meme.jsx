d9eaa86cff434601df6bce5b71a9ed1f347216cf
Silas Wolf <azolyth@gmail.com>
2022-10-18 15:06:40 -0700

    Setup random url generation from memesData.js

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
+// Data
+import memesData from '../memesData';
+
 const Meme = () => {
+  const handleClick = () => {
+    const randomUrl = Math.floor(Math.random() * memesData.data.memes.length);
+    console.log(memesData.data.memes[randomUrl].url);
+  };
+
   return (
     <main>
-      <form className='meme-form' action='submit'>
+      <div className='meme-form' action='submit'>
         <input className='form-input' type='text' placeholder='Top text' />
         <input className='form-input' type='text' placeholder='Bottom text' />
-        <button className='meme-btn gradient'>Get a new meme image</button>
-      </form>
+        <button className='meme-btn gradient' onClick={handleClick}>
+          Get a new meme image
+        </button>
+      </div>
     </main>
   );
 };
