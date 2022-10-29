87c1cd018c2b50ae84a6e9d3f00ff02e1f613bf2
Silas Wolf <azolyth@gmail.com>
2022-10-28 18:47:42 -0700

    Setup onChange handler for top and bottom text

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -9,10 +9,11 @@ const Meme = () => {
     bottomText: '',
     randomImage: 'https://i.imgflip.com/1bij.jpg',
   });
+
   const [allMemeImages, setAllMemeImages] = useState(memesData);
 
   const getMemeImage = () => {
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     const url = memesArray[randomNumber].url;
 
@@ -21,16 +22,45 @@ const Meme = () => {
     });
   };
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return { ...prevMeme, [name]: value };
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <main>
       <div className='meme-form' action='submit'>
-        <input className='form-input' type='text' placeholder='Top text' />
-        <input className='form-input' type='text' placeholder='Bottom text' />
+        <input
+          className='form-input'
+          type='text'
+          placeholder='Top text'
+          name='topText'
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          className='form-input'
+          type='text'
+          placeholder='Bottom text'
+          name='bottomText'
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className='meme-btn gradient' onClick={getMemeImage}>
           Get a new meme image
         </button>
       </div>
-      <img className='meme-img' src={meme.randomImage} />
+      <div className='meme'>
+        <img className='meme-img' src={meme.randomImage} />
+        <h2 className='meme-text top'>{meme.topText}</h2>
+        <h2 className='meme-text bottom'>{meme.bottomText}</h2>
+      </div>
     </main>
   );
 };
